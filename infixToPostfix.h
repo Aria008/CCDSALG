db31a0aece01c2ce1526ec6e7aa5f39f0a3f6c04
Aria008 <106798911+Aria008@users.noreply.github.com>
2024-06-20 19:24:33 +0800

    Update infixToPostfix.h

--- infixToPostfix.h
+++ infixToPostfix.h
@@ -1,7 +1,7 @@
 #ifndef INFIXTOPOSTFIX_H
 #define INFIXTOPOSTFIX_H
 
-#include "queue.h" // TO CHANGE INTO QUEUE HEADER FILE
+#include "queue.h" //REMINDER: TO CHANGE INTO QUEUE HEADER FILE
 
 void infixToPostfix(char* infix, Queue *postfixQueue);
 
