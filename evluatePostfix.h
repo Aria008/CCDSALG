e702329fe78879899589ed73ae4cae279c8f26e4
Aria008 <106798911+Aria008@users.noreply.github.com>
2024-06-20 22:12:18 +0800

    Create evaluatePostfix.h
    header file for evaluatePostfix

---
+++ evluatePostfix.h
@@ -0,0 +1,8 @@
+#ifndef EVALUATEPOSTFIX_H
+#define EVALUATEPOSTFIX_H
+
+#include "queue.h"// REMINDER: TO CHECK IF SAME NAME WITH QUEUE FILE
+
+int evaluatePostfix(Queue *postfixQueue, char *error);
+
+#endif
